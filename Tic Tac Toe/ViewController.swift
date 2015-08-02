efb6d6031bcc7cee18072da606bea795ef28be43
Bojie Jiang <jiangbojie@gmail.com>
2015-08-01 20:28:44 -0700

    Add the layout

--- Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
